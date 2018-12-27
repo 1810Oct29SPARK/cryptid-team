c751b4c2f523d20f6efad189d70d16409cc5814e
Max Couch <caxmouch@gmail.com>
2018-12-27 11:30:24 -0500

    servlets partially complete

--- Cryptids/src/main/java/servlets/Cryptid.java
+++ Cryptids/src/main/java/servlets/Cryptid.java
@@ -1,51 +1,43 @@
 package servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import service.CryptidService;
+import service.CryptidServiceImpl;
 
 /**
  * Servlet implementation class Cryptid
  */
 public class Cryptid extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private ObjectMapper om;
+    private CryptidService cryptidService;
+	
+	
     public Cryptid() {
         super();
-        // TODO Auto-generated constructor stub
+        cryptidService = new CryptidServiceImpl();
+        om = new ObjectMapper();
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int id = Integer.parseInt(request.getParameter("id"));
+		response.getWriter().write(om.writeValueAsString(cryptidService.getAccountsById(id)));
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");
+		
 	}
-
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
-
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
---
+++ cryptid.sql
@@ -0,0 +1,33 @@
+    create table Cryptid_Park (
+	parkid INT,
+	name VARCHAR(50)
+);
+insert into Cryptid_Park (parkid, name) values (1, 'Black-crowned night heron');
+
+create table Cryptid_Biome (
+	biomeId INT,
+	name VARCHAR(50),
+	foliage VARCHAR(50)
+);
+insert into Cryptid_Biome (biomeId, name, foliage) values (1, 'Eagle, white-bellied sea', null);
+insert into Cryptid_Biome (biomeId, name, foliage) values (2, 'Raccoon, common', null);
+insert into Cryptid_Biome (biomeId, name, foliage) values (3, 'Marshbird, brown and yellow', null);
+insert into Cryptid_Biome (biomeId, name, foliage) values (4, 'Weaver, lesser masked', null);
+
+create table CRYPTID_CRYPTIDS (
+	CryptidId INT,
+	NAME VARCHAR(50),
+	DIET VARCHAR(50),
+	AVG_1WEIGHT INT,
+	CLASS VARCHAR(50)
+);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (1, 'Herrick', null, 218, null);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (2, 'Sena', null, 402, null);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (3, 'Sholom', null, 379, null);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (4, 'Rosa', null, 462, null);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (5, 'Cobb', null, 322, null);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (6, 'Mickie', null, 488, null);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (7, 'Lily', null, 347, null);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (8, 'Shae', null, 164, null);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (9, 'Ira', null, 212, null);
+insert into CRYPTID_CRYPTIDS (CryptidId, NAME, DIET, AVG_1WEIGHT, CLASS) values (10, 'Bartlet', null, 332, null);
